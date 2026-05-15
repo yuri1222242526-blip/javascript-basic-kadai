7476cfbcef4d395080e7d00a90e35167478a99e4
yuri1222242526-blip <yuri1222242526@gmail.com>
2026-05-16 08:55:40 +0900

    Delete kadai_005 directory

--- kadai_005/js/dist/main.dev.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var weight = 68;
-var height = 1.7;
-var bmi = weight / (height * height);
-console.log(bmi);
--- kadai_005/js/main.js
+++
@@ -1,4 +0,0 @@
-let weight = 68;
-let height = 1.7;
-let bmi = weight / (height * height);
-console.log(bmi);
